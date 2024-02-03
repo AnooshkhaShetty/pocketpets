18598d79ec8496c579a093a1da10bf565e898efe
Madurya Suresh <maduryasuresh@wifi-131-179-9-94.host.ucla.edu>
2024-02-03 12:34:22 -0800

    stack navigator. iimport screens into app.js and load stack screens

---
+++ PocketPets/App.js
@@ -0,0 +1,39 @@
+//import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { StatusBar } from 'expo-status-bar';
+import Home from './components/Home';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+
+//import StackNavigator from "./StackNavigator";
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+
+  {/* <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View> */}
+  return (
+
+    <NavigationContainer> 
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home}></Stack.Screen>
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ 
---
+++ PocketPets/components/Home.jsx
@@ -0,0 +1,24 @@
+import {
+    View,
+    Text,
+    SafeAreaView,
+    Pressable,
+    Image,
+    ScrollView,
+    StyleSheet,
+  } from "react-native";
+  import React, { useState } from "react";
+  import { Ionicons } from "@expo/vector-icons";
+  import { MaterialIcons } from "@expo/vector-icons";
+  import { MaterialCommunityIcons } from "@expo/vector-icons";
+  import { useNavigation } from "@react-navigation/native";
+
+export default function Home() {
+  return(
+
+    <View>
+      <Text> Hello world! </Text>
+    </View>
+  )
+}
+
