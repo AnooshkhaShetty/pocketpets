fbc8a4e55a46931f7e8d281c7afd5c025f5ef5fd
Madurya Suresh <maduryasuresh@wifi-131-179-9-94.host.ucla.edu>
2024-02-03 13:36:41 -0800

    example component

--- PocketPets/components/Home.jsx
+++ PocketPets/components/Home.jsx
@@ -12,13 +12,44 @@ import {
   import { MaterialIcons } from "@expo/vector-icons";
   import { MaterialCommunityIcons } from "@expo/vector-icons";
   import { useNavigation } from "@react-navigation/native";
+  //import { useFonts } from 'expo-font';
 
 export default function Home() {
-  return(
 
-    <View>
-      <Text> Hello world! </Text>
-    </View>
+  return(
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.center_text}>
+          <Text style={styles.title}> Hello world! </Text>
+          <View style={styles.rectangle}>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: "#D9C6C7",
+      height: '100%',
+      width: '100%',
+    },
+  title: {
+    fontSize: 18,
+    fontFamily: "Sans-Serif",
+    fontWeight: 400,
+  },
+  center_text: {
+    marginTop: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  rectangle: {
+    width: 200,
+    height: 100,
+    backgroundColor: "#FFF",
+  }
+  })
